4fc2fed123ec4d6848402e6f83b64738f5fb4de6
Khaya Madlala <madlalakhaya@gmail.com>
2026-05-28 23:26:45 +0200

    clerk fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,20 +52,20 @@ export const viewport: Viewport = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <html
-        lang="en-ZA"
-        className={`${geistSans.variable} ${geistMono.variable} h-full`}
-        suppressHydrationWarning
-      >
-        <head>
-          <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        </head>
-        <body className="min-h-full flex flex-col antialiased bg-[#F7F3EA] text-[#1C1C1C]">
+    <html
+      lang="en-ZA"
+      className={`${geistSans.variable} ${geistMono.variable} h-full`}
+      suppressHydrationWarning
+    >
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+      <body className="min-h-full flex flex-col antialiased bg-[#F7F3EA] text-[#1C1C1C]">
+        <ClerkProvider>
           <PWARegister />
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   )
 }
